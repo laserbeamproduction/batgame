4b3fe42444a045a06fb42cbb6d1c8f8a053e60e9
Thomas van Opstal <opstal.thomas@gmail.com>
2016-09-06 15:23:07 +0200

    modified move script

--- BatCave/Assets/Scripts/Util/SimpleMoveScript.cs
+++ BatCave/Assets/Scripts/Util/SimpleMoveScript.cs
@@ -3,25 +3,38 @@ using System.Collections;
 using UnityEngine.Events;
 
 public class SimpleMoveScript : MonoBehaviour {
-
-    public float speed;
-    public Vector3 direction;
+    public Vector2 Speed = new Vector2(0.1f, 0.1f);
+    private Rigidbody2D rb;
+    //public float speed;
+    //public Vector3 direction;
 
     void Start() {
-        this.speed = SaveLoadController.GetInstance().GetPlayer().GetSpeed();
-        EventManager.StartListening(EventTypes.PLAYER_SPEED_CHANGED, OnSpeedChanged);
+        //this.speed = SaveLoadController.GetInstance().GetPlayer().GetSpeed();
+        //EventManager.StartListening(EventTypes.PLAYER_SPEED_CHANGED, OnSpeedChanged);
+        rb = GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.velocity = new Vector2(Speed.x, Speed.y);
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate() {
-        this.gameObject.transform.Translate(direction * speed * Time.deltaTime);
+        //this.gameObject.transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    void Update() {
+        if (rb == null)
+        {
+            transform.Translate(Speed.x, Speed.y, 0);
+        }
     }
 
     void OnSpeedChanged() {
-        this.speed = SaveLoadController.GetInstance().GetPlayer().GetSpeed();
+        //this.speed = SaveLoadController.GetInstance().GetPlayer().GetSpeed();
     }
 
     void OnDestroy() {
-        EventManager.StopListening(EventTypes.PLAYER_SPEED_CHANGED, OnSpeedChanged);
+        //EventManager.StopListening(EventTypes.PLAYER_SPEED_CHANGED, OnSpeedChanged);
     }
 }
