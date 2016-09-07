fcbd29a18edd279ad1632c00bc509fa260926b98
Sam Meyer <sammie_8288@hotmail.com>
2016-09-07 21:37:58 +0200

    Added blood particles on hitting a fly

--- BatCave/Assets/Scripts/Spawnables/PickUps/Fly/FlyController.cs
+++ BatCave/Assets/Scripts/Spawnables/PickUps/Fly/FlyController.cs
@@ -4,18 +4,52 @@ using System;
 
 public class FlyController : MonoBehaviour {
     public bool markedForDestroy = false;
-    
-    void OnCollisionEnter2D(Collision2D col) {
-        if (col.gameObject.tag == "Player")
-        {
-            EventManager.TriggerEvent(EventTypes.FLY_PICK_UP);
-            markedForDestroy = true;
-            gameObject.SetActive(false);
+    private ParticleSystem particle;
+    private SpriteRenderer spriteRenderer;
+
+    private bool isHitByPlayer;
+    private bool isEmittingBlood;
+
+    void Start() {
+        particle = GetComponent<ParticleSystem>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        if (isHitByPlayer) {
+            if (!isEmittingBlood) {
+                StartBloodParticles();
+                isEmittingBlood = true;
+                spriteRenderer.enabled = false;
+            } else {
+                if (!particle.isPlaying) {
+                    OnParticleAnimFinished();
+                }
+            }
         }
 
-        if (col.gameObject.tag == "CleanUp") {
-            markedForDestroy = true;
-            gameObject.SetActive(false);
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (!isHitByPlayer) {
+            if (col.gameObject.tag == "Player") {
+                EventManager.TriggerEvent(EventTypes.FLY_PICK_UP);
+                isHitByPlayer = true;
+            }
+
+            if (col.gameObject.tag == "CleanUp") {
+                markedForDestroy = true;
+                gameObject.SetActive(false);
+            }
         }
+    }
+
+    void StartBloodParticles() {
+        particle.Play();
+    }
+
+    void OnParticleAnimFinished() {
+        markedForDestroy = true;
+        gameObject.SetActive(false);
     }
 }
