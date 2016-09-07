e4a524fa264f13b7fb75208b359877becb80fe73
Sam Meyer <sammie_8288@hotmail.com>
2016-09-07 12:16:10 +0200

    Added skill slider prefab with EventTypes

--- BatCave/Assets/Scripts/EventManager/EventTypes.cs
+++ BatCave/Assets/Scripts/EventManager/EventTypes.cs
@@ -12,6 +12,7 @@ public class EventTypes {
 
     public static string FLY_PICK_UP = "flyPickUp";
     public static string ECHO_USED = "echoUsed";
+    public static string SKILL_VALUE = "echoUsed";
     public static string SET_DAY_TIME = "setDayTime";
     public static string SET_NIGHT_TIME = "setNightTime";
     public static string SHAPE_SHIFT = "shapeShift";
