5107f2960dcdcc32c5a4f8086a10bf43779c53b9
Sam Meyer <sammie_8288@hotmail.com>
2016-09-11 22:34:32 +0200

    Adjusted achievement values in code

--- BatCave/Assets/Scripts/Util/AchievementChecker.cs
+++ BatCave/Assets/Scripts/Util/AchievementChecker.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class AchievementChecker : MonoBehaviour {
 
     public static void CheckForEndlessScoreAchievement(float score) {
-        if (score >= 3000)
-            GooglePlayHelper.GetInstance().UnlockAchievement(GPGSConstant.achievement_chicken_wings);
         if (score >= 5000)
-            GooglePlayHelper.GetInstance().UnlockAchievement(GPGSConstant.achievement_buffalo_wings);
+            GooglePlayHelper.GetInstance().UnlockAchievement(GPGSConstant.achievement_chicken_wings);
         if (score >= 10000)
-            GooglePlayHelper.GetInstance().UnlockAchievement(GPGSConstant.achievement_wingin_it);
+            GooglePlayHelper.GetInstance().UnlockAchievement(GPGSConstant.achievement_buffalo_wings);
         if (score >= 20000)
+            GooglePlayHelper.GetInstance().UnlockAchievement(GPGSConstant.achievement_wingin_it);
+        if (score >= 50000)
             GooglePlayHelper.GetInstance().UnlockAchievement(GPGSConstant.achievement_ride_the_wings_of_pestilance);
     }
 
