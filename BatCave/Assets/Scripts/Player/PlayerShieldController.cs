33081cf573e69c83de0a7822f0c4e8c8c90cd78d
Sam Meyer <sammie_8288@hotmail.com>
2016-09-14 12:54:32 +0200

    Finished shield power up

---
+++ BatCave/Assets/Scripts/Player/PlayerShieldController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShieldController : MonoBehaviour {
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = false;
+        EventManager.StartListening(EventTypes.PLAYER_SHIELD_PICKUP, OnShieldActivated);
+        EventManager.StartListening(EventTypes.PLAYER_SHIELD_ENDED, OnsShieldEnded);
+    }
+
+    void OnDestroy() {
+        EventManager.StopListening(EventTypes.PLAYER_SHIELD_PICKUP, OnShieldActivated);
+        EventManager.StopListening(EventTypes.PLAYER_SHIELD_ENDED, OnsShieldEnded);
+    }
+
+    void OnShieldActivated() {
+        spriteRenderer.enabled = true;
+    }
+
+    void OnsShieldEnded() {
+        spriteRenderer.enabled = false;
+    }
+}
