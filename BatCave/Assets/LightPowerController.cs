5df90708841eff89a0b0dd4ca5ab9d23d9e9d7fa
Sam Meyer <sammie_8288@hotmail.com>
2016-09-14 14:02:24 +0200

    Added pretty particles on light power picked up

---
+++ BatCave/Assets/LightPowerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightPowerController : MonoBehaviour {
+
+    private ParticleSystem particleSystem;
+
+    void Start() {
+        particleSystem = GetComponent<ParticleSystem>();
+        EventManager.StartListening(EventTypes.SET_DAY_TIME, OnLightPowerPickedUp);
+    }
+
+    void OnDestroy() {
+        EventManager.StopListening(EventTypes.SET_DAY_TIME, OnLightPowerPickedUp);
+    }
+
+    void OnLightPowerPickedUp() {
+        particleSystem.Play();
+    }
+}
