20b3b1f51cab389f4f68b2180a1755c0555789f8
Sam Meyer <sammie_8288@hotmail.com>
2016-09-14 15:03:10 +0200

    Added blinking for shield power

--- BatCave/Assets/Scripts/Player/PlayerShieldController.cs
+++ BatCave/Assets/Scripts/Player/PlayerShieldController.cs
@@ -4,10 +4,18 @@ using System.Collections;
 public class PlayerShieldController : MonoBehaviour {
 
     private SpriteRenderer spriteRenderer;
+    public float blinkInterval = 0.5f;
+    public bool powerActive;
+    public ShieldBooster boosterScript;
+
+    private float blinkIntervalCounter;
+    private bool isBlinking;
+    private float shieldDuration;
 
     void Start() {
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.enabled = false;
+        shieldDuration = boosterScript.shieldDuration;
         EventManager.StartListening(EventTypes.PLAYER_SHIELD_PICKUP, OnShieldActivated);
         EventManager.StartListening(EventTypes.PLAYER_SHIELD_ENDED, OnsShieldEnded);
     }
@@ -19,9 +27,32 @@ public class PlayerShieldController : MonoBehaviour {
 
     void OnShieldActivated() {
         spriteRenderer.enabled = true;
+        powerActive = true;
     }
 
     void OnsShieldEnded() {
+        isBlinking = false;
+        blinkIntervalCounter = 0;
         spriteRenderer.enabled = false;
+        powerActive = false;
+    }
+
+
+    void Update() {
+        if (shieldDuration < 2f && !isBlinking && powerActive) {
+            isBlinking = true;
+        }
+
+        if (isBlinking) {
+            blinkIntervalCounter += Time.deltaTime;
+            if (blinkIntervalCounter >= blinkInterval) {
+                ToggleSprite();
+                blinkIntervalCounter = 0;
+            }
+        }
+    }
+
+    void ToggleSprite() {
+        spriteRenderer.enabled = !spriteRenderer.enabled;
     }
 }
