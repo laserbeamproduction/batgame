108798885b6b46a606fb5210417cb1c495cef673
Zammstein <sammie_8288@hotmail.com>
2016-09-19 21:04:41 +0200

    Added fire particles for speed boost jetpack

---
+++ BatCave/Assets/PlayerBoostParticles.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerBoostParticles : MonoBehaviour {
+
+    private ParticleSystem particleSystem;
+
+	void Start () {
+        particleSystem = GetComponent<ParticleSystem>();
+        EventManager.StartListening(EventTypes.PLAYER_SPEED_ENDED, OnSpeedBoostEnded);
+        EventManager.StartListening(EventTypes.PLAYER_SPEED_PICKUP, OnSpeedBoostPickedUp);
+    }
+
+    void OnDestroy () {
+        EventManager.StopListening(EventTypes.PLAYER_SPEED_ENDED, OnSpeedBoostEnded);
+        EventManager.StopListening(EventTypes.PLAYER_SPEED_PICKUP, OnSpeedBoostPickedUp);
+    }
+
+    private void OnSpeedBoostPickedUp(object arg0)
+    {
+        particleSystem.enableEmission = true;
+        particleSystem.loop = true;
+        particleSystem.Play();
+    }
+
+    private void OnSpeedBoostEnded(object arg0)
+    {
+        particleSystem.enableEmission = false;
+        particleSystem.loop = false;
+        particleSystem.Stop();
+    }
+}
