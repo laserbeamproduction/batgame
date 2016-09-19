8a4cfb7aaa29884f7766c0e1137309c83d733941
Sam Meyer <sammie_8288@hotmail.com>
2016-09-19 15:18:36 +0200

    Added begin for new SpawnSystem

---
+++ BatCave/Assets/Scripts/Level/Endless/SpawnSystem/Model/BatchModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+public class BatchModel {
+
+    private List<SpawnPointModel> spawnPoints;
+
+    public BatchModel(List<SpawnPointModel> spawnPoints) {
+        this.spawnPoints = spawnPoints;
+    }
+}
---
+++ BatCave/Assets/Scripts/Level/Endless/SpawnSystem/Model/SpawnPointModel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SpawnPointModel {
+
+    private Vector3 position;
+    private GameObject item;
+
+    public SpawnPointModel(Vector3 position) {
+        this.position = position;
+    }
+
+    public void SetItem(GameObject item) {
+        this.item = item;
+    }
+
+    public bool IsSlotTaken() {
+        return this.item != null;
+    }
+}
