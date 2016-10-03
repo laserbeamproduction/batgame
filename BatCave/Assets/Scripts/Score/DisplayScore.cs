713bb9da90908bdb33c36a9661cc79e7e4d00350
Zammstein <sammeyer1994@hotmail.com>
2016-10-03 09:19:43 +0200

    Added coin pickup animation on UI

--- BatCave/Assets/Scripts/Score/DisplayScore.cs
+++ BatCave/Assets/Scripts/Score/DisplayScore.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using System;
 
 public class DisplayScore : MonoBehaviour {
     public ScoreCalculator score;
 
     private Text text;
     public Text coinText;
+    public Image coinImage;
     private bool isPaused;
+
+    private Animator coinAnimator;
 
     private EndlessSessionSave ess;
 
@@ -16,12 +20,18 @@ public class DisplayScore : MonoBehaviour {
         text = GetComponent<Text>();
         text.text = "0";
         ess = SaveLoadController.GetInstance().GetEndlessSession();
+        coinAnimator = coinImage.GetComponent<Animator>();
         EventManager.StartListening(EventTypes.GAME_RESUME, OnGameResume);
         EventManager.StartListening(EventTypes.GAME_PAUSED, OnGamePaused);
+        EventManager.StartListening(PowerupEvents.PLAYER_COIN_PICKUP, OnCoinPickedUp);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void OnCoinPickedUp(object arg0) {
+        coinAnimator.SetTrigger("Play");
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (!isPaused) {
             text.text = Mathf.FloorToInt(score.playerScore).ToString();
             coinText.text = ess.GetTotalCoinsCollected().ToString();
@@ -31,6 +41,7 @@ public class DisplayScore : MonoBehaviour {
     void OnDestroy() {
         EventManager.StopListening(EventTypes.GAME_RESUME, OnGameResume);
         EventManager.StopListening(EventTypes.GAME_PAUSED, OnGamePaused);
+        EventManager.StopListening(PowerupEvents.PLAYER_COIN_PICKUP, OnCoinPickedUp);
     }
 
     void OnGamePaused(object arg0) {
